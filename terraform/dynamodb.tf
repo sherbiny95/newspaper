57f1e695892d0a0e86dacd1aeeb341774e680228
Amr Elsherbini <aelsherbini@deloitte.nl>
2023-09-09 14:45:51 +0200

    lambda and api

--- terraform/dynamodb.tf
+++ terraform/dynamodb.tf
@@ -1,4 +1,5 @@
 resource "aws_dynamodb_table" "newspaper_articles" {
+  #  AWS DynamoDB tables are automatically encrypted at rest with an AWS-owned Customer Master Key if this argument isn't specified
   name         = "newspaper-articles"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "Title"
