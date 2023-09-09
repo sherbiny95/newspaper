bc258b8690664c3b457560650d18c32ae80f34c0
Amr Elsherbini <aelsherbini@deloitte.nl>
2023-09-09 20:19:17 +0200

    fix api

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -30,16 +30,6 @@ resource "aws_api_gateway_integration" "news_lambda_integration" {
   uri                     = module.lambda_news.lambda_function_invoke_arn
 }
 
-# resource "aws_api_gateway_integration" "news_lambda_integration" {
-#   rest_api_id = aws_api_gateway_rest_api.news_api.id
-#   resource_id = aws_api_gateway_resource.news.id
-#   http_method = aws_api_gateway_method.get_news.http_method
-
-#   integration_http_method = "POST"
-#   type                    = "AWS_PROXY"
-#   uri                     = module.lambda_get.lambda_function_invoke_arn
-# }
-
 resource "aws_api_gateway_resource" "newsitem" {
   rest_api_id = aws_api_gateway_rest_api.news_api.id
   parent_id   = aws_api_gateway_rest_api.news_api.root_resource_id
