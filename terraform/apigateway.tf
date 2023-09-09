72f04e3abb91f3f758447fd9572b51fc577951af
Amr Elsherbini <aelsherbini@deloitte.nl>
2023-09-09 18:21:49 +0200

    fix api

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -32,7 +32,7 @@ resource "aws_api_gateway_integration" "news_lambda_integration" {
 
 resource "aws_api_gateway_resource" "newsitem" {
   rest_api_id = aws_api_gateway_rest_api.news_api.id
-  parent_id   = aws_api_gateway_resource.news.id
+  parent_id   =  aws_api_gateway_rest_api.news_api.root_resource_id
   path_part   = "newsitem"
 }
 
